408efcfa2fc69a39cc5dbbfcad7d7a6c8f96c4d9
Adrian Perez <adrian1804perez@gmail.com>
2024-10-11 16:20:57 +0000

    Add annotations

--- c_list.c
+++ c_list.c
@@ -11,11 +11,12 @@ linked_list *new_linked_list()
     return ret;
 };
 
+// You need to free each cell and then the list struct itself
 void free_linked_list(linked_list *l)
 {
     linked_list_cell *at = l->head;
     linked_list_cell *tmp = at;
-    while (at)
+    while (at) // While you are at a node
     {
         tmp = at->next;
         free(at);
@@ -27,8 +28,10 @@ void free_linked_list(linked_list *l)
 void prepend(linked_list *l, void *data)
 {
     linked_list_cell *node = malloc(sizeof(linked_list_cell));
+    // Putting the node into place
     node->next = l->head;
     l->head = node;
+    // Setting and managing the meta data
     node->data = data;
     l->length += 1;
 }
@@ -37,6 +40,7 @@ void append(linked_list *l, void *data)
 {
     linked_list_cell *tmp = l->head;
     l->length += 1;
+    // Edge case where you start off with an empty list {
     if (l->head == NULL)
     {
         l->head = malloc(sizeof(linked_list_cell));
@@ -44,7 +48,8 @@ void append(linked_list *l, void *data)
         l->head->data = data;
         return;
     }
-    while (tmp->next != NULL)
+    // }
+    while (tmp->next != NULL) // When this condition is false you have reached the end
     {
         tmp = tmp->next;
     }
@@ -70,6 +75,7 @@ void *get_at(linked_list *l, size_t index)
     return NULL;
 }
 
+// Sets the given value at the given index. Will do nothing if index is out of range
 void set_at(linked_list *l, size_t index, void *data)
 {
     linked_list_cell *at = l->head;
